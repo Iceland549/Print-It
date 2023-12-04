95bd5618208c1ccf635147a88e49b134c0f3f96e
Nathrop <chaudenisbreiz@yahoo.fr>
2023-12-04 10:50:45 +0100

    clean code

--- assets/script.js
+++ assets/script.js
@@ -1,113 +1,94 @@
 // Tableau de slides avec des images et des textes associés
 const slides = [
-	{
-		"image":"./assets/images/slideshow/slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"./assets/images/slideshow/slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"./assets/images/slideshow/slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"./assets/images/slideshow/slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
-
+  {
+    image: "./assets/images/slideshow/slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "./assets/images/slideshow/slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "./assets/images/slideshow/slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "./assets/images/slideshow/slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 
 // Sélection des éléments du DOM
-const arrowLeft = document.querySelector('.arrow_left');
-const arrowRight = document.querySelector('.arrow_right');
-const sliderBullets = document.querySelector('.dots'); 
-// Count number of slides
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+const sliderBullets = document.querySelector(".dots");
 const numberOfSlides = slides.length;
-console.log('Nombre de slides :', numberOfSlides);
-
 
 // Index actuel de la slide affichée
 let currentIndex = 0;
 
 // Gestionnaire d'événements pour avancer à la slide suivante
-arrowRight.addEventListener('click', () => {
+arrowRight.addEventListener("click", () => {
   if (currentIndex === slides.length - 1) {
     currentIndex = 0; // Revenir à la première slide si c'est la dernière
   } else {
     currentIndex++; // Passer à la slide suivante
   }
-  
+
   updateSlide(currentIndex);
   updateDot(currentIndex);
 });
 
-
 // Gestionnaire d'événements pour revenir à la slide précédente
-arrowLeft.addEventListener('click', () => {
-  if (currentIndex === slides.length - 1) {
-    currentIndex = 0; // Revenir à la première slide si c'est la dernière
+arrowLeft.addEventListener("click", () => {
+  if (currentIndex === 0) {
+    currentIndex = slides.length - 1; // Revenir à la dernière slide si c'est la première
   } else {
     currentIndex--; // Passer à la slide suivante
   }
-  
+
   updateSlide(currentIndex);
   updateDot(currentIndex);
 });
 
-
 // Créer des dots pour chaque slide
 for (let i = 0; i < numberOfSlides; i++) {
-  const bullet = document.createElement('li'); // element li created
-  bullet.classList.add('dot'); // Class for all bullet points
+  const bullet = document.createElement("li"); // création des éléments li
+  bullet.classList.add("dot"); // Ajoute la classe dot pour chaque point
 
   if (i === 0) {
-    bullet.classList.add('dot_selected'); // Special class to first slide
+    bullet.classList.add("dot_selected"); // Ajoute la classe dot_selected pour la slide actuelle
   }
 
-  sliderBullets.appendChild(bullet); // Add bullet point to parent
+  sliderBullets.appendChild(bullet); // Ajoute le bullet au parent sliderBullets
+
+  // Ajouter des écouteurs d'événements à chaque dot
+  bullet.addEventListener("click", () => {
+    updateSlide(i);
+    updateDot(i);
+  });
 }
-
-console.log('Bullet points created :', document.querySelectorAll('.dot').length);
-
 
 // Mettre à jour l'image et le texte de la slide actuelle
 function updateSlide(index) {
+  const sliderImage = document.querySelector(".slides-image");
+  const sliderText = document.querySelector(".slides-tagLine");
 
-	const sliderImage = document.querySelector('.slides-image');
-	const sliderText = document.querySelector('.slides-tagLine');
-
-    sliderImage.src = slides[index].image;
-    sliderText.innerHTML = slides[index].tagLine;
+  sliderImage.src = slides[index].image;
+  sliderText.innerHTML = slides[index].tagLine;
 }
 
 // Mettre à jour la sélection des dots
 function updateDot(index) {
-    const dots = document.querySelectorAll('.dot');
+  const dots = document.querySelectorAll(".dot");
 
-    // Ajouter des écouteurs d'événements à chaque dot
-    dots.forEach((dot, index) => {
-        dot.addEventListener('click', () => {
-          updateSlide(index);
-          updateDot(index);
-        });
-      });
-
-    // Mettre à jour la sélection visuelle des dots
-    dots.forEach((dot, i) => {
-        if (i === index) {
-            dot.classList.add('dot_selected');
-        } else {
-            dot.classList.remove('dot_selected');
-        }
-    });
+  // Mettre à jour la sélection visuelle des dots
+  dots.forEach((dot, i) => {
+    if (i === index) {
+      dot.classList.add("dot_selected");
+    } else {
+      dot.classList.remove("dot_selected");
+    }
+  });
 }
-
-
-
-// Afficher la première slide au chargement de la page
-updateSlide(currentIndex);
-updateDot(currentIndex);
-
-console.log(document.querySelector('.slides-image'))
